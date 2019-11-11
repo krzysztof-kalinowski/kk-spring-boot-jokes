62adb2e4ecdc8b43ee63cdc6c1980613c8b3ca76
Krzysztof Kalinowski <kalinowski.krzysztof@gmail.com>
2019-11-11 18:40:09 +0100

    joke services

---
+++ src/main/java/kkalinowski/springframework/joke/jokeapp/service/JokeService.java
@@ -0,0 +1,10 @@
+package kkalinowski.springframework.joke.jokeapp.service;
+
+/**
+ * Created by Krzysztof Kalinowski on 11/11/2019.
+ */
+
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/kkalinowski/springframework/joke/jokeapp/service/JokeServiceImpl.java
@@ -0,0 +1,23 @@
+package kkalinowski.springframework.joke.jokeapp.service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Krzysztof Kalinowski on 11/11/2019.
+ */
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
